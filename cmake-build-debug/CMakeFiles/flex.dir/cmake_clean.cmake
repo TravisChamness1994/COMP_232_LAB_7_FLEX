41a42a6b046837e17dadfef5b2e274605e259b7a
travischamness <travis_chamness1@my.vcccd.edu>
2020-03-20 12:56:29 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/flex.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flex.dir/lexer.c.o"
+  "CMakeFiles/flex.dir/parser.c.o"
+  "CMakeFiles/flex.dir/src/flex.c.o"
+  "flex"
+  "flex.pdb"
+  "lexer.c"
+  "parser.c"
+  "parser.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/flex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
